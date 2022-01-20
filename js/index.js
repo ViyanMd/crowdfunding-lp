9411610012e79a693d5752188ad0ee09501eb584
Viyan Md <viyanmd@192.168.1.2>
2022-01-20 20:57:41 +0300

    design ready/no functionality

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 //HAMBURGER MENU CLOSE - OPEN BUTTON 
 
 const menu = document.querySelector('.nav_bar');
@@ -12,6 +14,8 @@ menuBtn.addEventListener('click', () => {
 const modalOpenBtn = document.querySelectorAll('.btn_select');
 const modalCloseBtn = document.querySelector('.icon_close_modal');
 const modalBg = document.querySelector('.modal_bg');
+const modalThank = document.querySelector('.modal_thanks_bg');
+const submitBtn = document.querySelectorAll('.btn_form');
 
 modalOpenBtn.forEach(button => button.addEventListener('click', () => {
     modalBg.classList.add('modal_active');
@@ -21,42 +25,51 @@ modalCloseBtn.addEventListener('click', () => {
     modalBg.classList.remove('modal_active');
 });
 
+
+
+
 //BOOKMARK BTN ACTIVE
 
 const bookmarkBtn = document.querySelector('.btn_bookmark');
 const bookmarkCircle = document.querySelector('.btn_bookmark_circle');
 const bookmarkPath = document.querySelector('.btn_bookmark_path');
 const bookmarkText = document.querySelector('.btn_bookmark_text');
-const bookmarkIcon = [bookmarkBtn, bookmarkCircle, bookmarkPath,bookmarkText];
+const bookmarkIcon = [bookmarkBtn, bookmarkCircle, bookmarkPath, bookmarkText];
 
-
-bookmarkBtn.addEventListener('click', () => {
-    bookmarkIcon.forEach(el => el.classList.toggle('marked'));
+function bookmark(arr) {
+ bookmarkIcon.forEach(el => el.classList.toggle('marked'));
     if(bookmarkText.innerHTML === 'Bookmark') {
         bookmarkText.innerHTML = 'Bookmarked';
     } else {
         bookmarkText.innerHTML = "Bookmark";
     }
+}
+
+
+bookmarkBtn.addEventListener('click', () => {
+   bookmark(bookmarkIcon);
 });
 
 //MODAL SECTION OPTIONS ANIMATIONS 
 
 const radioBtn = document.querySelectorAll("input[type='radio']");
-const input = document.querySelectorAll('.option_input');
-let pledges = document.querySelectorAll('.options > *');
+const pledges = document.querySelectorAll('.options > *');
+
 
 function selectedPledge(target) {
     pledges.forEach(item => {
         if(item.classList.contains('active')) {
             item.classList.remove('active');
-        }
+        } 
         if(item.lastElementChild.classList.contains('active')) {
             item.lastElementChild.classList.remove('active');
         }
-    })
+    });
+
+    target.scrollIntoView({block: "center", behavior: "smooth"});
     target.closest('.option').classList.add("active");
     target.closest('.option').lastElementChild.classList.add('active');
-}
+};
 
 radioBtn.forEach(item => {
     item.addEventListener('change', (e) => {
